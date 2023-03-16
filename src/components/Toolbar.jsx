da7ce1320ef47f37cc64ba088eea9dffd8fcfbcd
Maxim Golikov <frystration@gmail.com>
2023-03-16 22:33:27 +0300

    brush and rect tools

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import "../styles/toolbar.scss";
+import Brush from "../tools/Brush";
+import toolState from "../store/toolState";
+import canvasState from "../store/canvasState";
+import Rect from "../tools/Rect";
 
 const Toolbar = () => {
     return (
         <div className="toolbar">
-            <button className="toolbar__btn brush"></button>
+            <button className="toolbar__btn brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+            <button className="toolbar__btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></button>
+            <button className="toolbar__btn circle" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+            <button className="toolbar__btn eraser" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+            <button className="toolbar__btn line" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+            <input style={{marginLeft: 10}} type="color"></input>
+            <button className="toolbar__btn undo" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+            <button className="toolbar__btn redo" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+            <button className="toolbar__btn save"onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
         </div>
     );
 };
